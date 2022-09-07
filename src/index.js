7fd977b1c1604aa61281fe72a42f0e53c2f36464
iSIayer <gamerspeed86@gmail.com>
2022-09-07 15:44:37 +0300

    add folders to start

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import { theme } from './theme/theme';
+import { App } from 'App';
+import 'modern-normalize';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
---
+++ src/theme/theme.js
@@ -0,0 +1,7 @@
+export const theme = {
+  colors: {
+    text: '#a1a1a1',
+    gray: '#797d7f',
+    accent: 'orange',
+  },
+};
